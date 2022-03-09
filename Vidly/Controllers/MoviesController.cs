5a597fbeba5364865bf4aab96cb8cc7452e66d66
EddyBautista <bautista.j.eddy@gmail.com>
2022-03-09 07:02:57 -0600

    working through sql migration

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,15 +10,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            Movie movie = new Movie() { Id = 1, Name = "Scream" };
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer{ Name = "Customer 1"},
-                new Customer{ Name = "Customer 2"},
-
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -26,14 +41,11 @@ namespace Vidly.Controllers
                 Movie = movie,
                 Customers = customers
             };
+
             return View(viewModel);
         }
 
-        //[Route("movies/released/{year:\\d}/{month:regex(\\d{2}}")]
-        //public ActionResult ByReleaseDate(int year, int month)
-        //{
-        //    return Content(year + " / " + month);
 
-        //}
+
     }
 }
---
+++ Vidly/Migrations/202203091239421_PopulateMembershipTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            // Seeding the DB with MembershipTypes
+            // Pay as you go
+            Sql("INSERT INTO MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate)" +
+                "VALUES (1, 0, 0, 0)");
+            // Monthly
+            Sql("INSERT INTO MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate)" +
+                "VALUES (2, 30, 1, 10)");
+            // Quartly
+            Sql("INSERT INTO MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate)" +
+                "VALUES (3, 90, 3, 15)");
+            // Yearly 
+            Sql("INSERT INTO MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate)" +
+                "VALUES (4, 300, 12, 20)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
